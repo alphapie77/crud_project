7139d584e4479aebf6233c3a13f8c755ab667306
Sabbir Hossain <122679080+alphapie77@users.noreply.github.com>
2024-12-06 17:12:34 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "crud_app";
+$port = 3307; // Specify the port
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
